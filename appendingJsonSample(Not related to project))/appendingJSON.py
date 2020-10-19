64a2758d780677f58ab36535528dd216294a3a2b
Aditya Choudhary <adityachoudhary54@gmail.com>
2020-10-19 23:49:36 +0530

    Example of how to append json in a dataframe

---
+++ appendingJsonSample(Not related to project))/appendingJSON.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import json
+data_1 = {
+    "id":1,
+    "key":"vbkhcbkdscnjkbvchadscebjl"
+}
+data_2 = {
+    "id":2,
+    "key":"cnxksafhnaldcmladsnvjkascxlm"
+}
+data_3 = {
+    "id": 3,
+    "key": "cnxkasdasxsaxsafhnaldcmladsnvjkascxlm"
+}
+
+df = pd.DataFrame()
+df = df.append(data_1,ignore_index=True)
+df = df.append(data_2,ignore_index=True)
+print(df)
+df.to_csv("junk.csv",index=False)
+
+df = pd.read_csv("junk.csv")
+df = df.append(data_3,ignore_index=True)
+print(df)
+print(df['id'])
+df.to_csv("junk.csv", index=False)
